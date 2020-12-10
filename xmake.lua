54e4e03f42e4525161ed8741ac8448550703e2f6
alei <187352451@qq.com>
2020-12-10 08:11:49 +0800

    修改编译条件

--- xmake.lua
+++ xmake.lua
@@ -14,12 +14,15 @@ set_strip("all")
 
 if is_plat("linux") then
     add_defines("LUA_USE_LINUX=1")
-    add_links("dl", "readline")
 end
 
 target("lualib-5.3.6") 
-    set_kind("static")    
-    add_headerfiles("lua-5.3.6/src/*.h")
+    set_kind("shared") 
+    add_headerfiles("lua-5.3.6/src/*.h") 
+    add_rules("binaryrule")
+    if is_plat("mingw") then 
+        add_defines("LUA_BUILD_AS_DLL")
+    end     
     add_files("lua-5.3.6/src/lapi.c"
     	, "lua-5.3.6/src/lcode.c"
     	, "lua-5.3.6/src/lctype.c"
@@ -57,26 +60,65 @@ target("lualib-5.3.6")
 
 
 target("lua")  
- 	add_deps("lualib-5.3.6")
     set_kind("binary")
+    add_deps("lualib-5.3.6")
+    add_links("lualib-5.3.6")
     add_files("lua-5.3.6/src/lua.c")
-	add_links("lualib-5.3.6")
 	add_rules("binaryrule")
+    if is_plat("linux") then
+        add_links("dl", "readline")
+    end
 
 target("luac")  
- 	add_deps("lualib-5.3.6")
     set_kind("binary")
-    add_files("lua-5.3.6/src/luac.c")
-	add_links("lualib-5.3.6")
+    add_files("lua-5.3.6/src/lapi.c"
+        , "lua-5.3.6/src/lcode.c"
+        , "lua-5.3.6/src/lctype.c"
+        , "lua-5.3.6/src/ldebug.c"
+        , "lua-5.3.6/src/ldo.c"
+        , "lua-5.3.6/src/ldump.c"
+        , "lua-5.3.6/src/lfunc.c"
+        , "lua-5.3.6/src/lgc.c"
+        , "lua-5.3.6/src/llex.c"
+        , "lua-5.3.6/src/lmem.c"
+        , "lua-5.3.6/src/lobject.c"
+        , "lua-5.3.6/src/lopcodes.c"
+        , "lua-5.3.6/src/lparser.c"
+        , "lua-5.3.6/src/lstate.c"
+        , "lua-5.3.6/src/lstring.c"
+        , "lua-5.3.6/src/ltable.c"
+        , "lua-5.3.6/src/ltm.c"
+        , "lua-5.3.6/src/lundump.c"
+        , "lua-5.3.6/src/lvm.c"
+        , "lua-5.3.6/src/lzio.c"
+        , "lua-5.3.6/src/lauxlib.c"
+        , "lua-5.3.6/src/lbaselib.c"
+        , "lua-5.3.6/src/lbitlib.c"
+        , "lua-5.3.6/src/lcorolib.c"
+        , "lua-5.3.6/src/ldblib.c"
+        , "lua-5.3.6/src/liolib.c"
+        , "lua-5.3.6/src/lmathlib.c"
+        , "lua-5.3.6/src/loslib.c"
+        , "lua-5.3.6/src/lstrlib.c"
+        , "lua-5.3.6/src/ltablib.c"
+        , "lua-5.3.6/src/lutf8lib.c"
+        , "lua-5.3.6/src/loadlib.c"
+        , "lua-5.3.6/src/linit.c"
+        , "lua-5.3.6/src/luac.c"
+        )    
 	add_rules("binaryrule")
+    if is_plat("linux") then
+        add_links("dl", "readline")
+    end
+
 
 rule("binaryrule")
-    set_extensions("", ".exe") 
+    set_extensions("", ".exe", ".so", ".dll") 
  	after_package(function (target)
         local targetfile = target:targetfile() 
         local extstr = path.extension(targetfile)
         local targetdir = target:targetdir() 
-        local filename = target:name() .. extstr 
+        local filename = path.filename(targetfile)
 
         local destdir = "../000_packages/lualib-5.3.6.pkg/$(plat)/$(arch)/bin/$(mode)"
         if not os.exists(destdir) then os.mkdir(destdir) end 
